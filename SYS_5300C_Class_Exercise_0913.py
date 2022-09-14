1a816c3104017775a129169e7d9564054f61b106
zhouzhouce <zhouzhouce@gmail.com>
2022-09-13 22:54:41 -0400

    Update SYS_5300C_Class_Exercise_0913.py

--- SYS_5300C_Class_Exercise_0913.py
+++ SYS_5300C_Class_Exercise_0913.py
@@ -1,7 +1,11 @@
 var1 = int(input('Input the first number:'))
 var2 = int(input('Input the second number:'))
 
-print(var1 + var2)
-print(var1 - var2)
-print(var1 * var2)
-print(var1 / var2)
+ans1 = var1 + var2
+
+print(ans1)
+
+var3 = int(input('Input the third number:'))
+
+ans2 = ans1 + var3
+print(ans2)
