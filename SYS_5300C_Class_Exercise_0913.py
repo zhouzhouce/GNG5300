9700465fc77444fa94590cb831a65260235a4e38
lilyrainbow <lilyrainbow@users.noreply.github.com>
2022-09-13 20:15:27 -0400

    Initial commit

---
+++ SYS_5300C_Class_Exercise_0913.py
@@ -0,0 +1,7 @@
+var1 = int(input('Input the first number:'))
+var2 = int(input('Input the second number:'))
+
+print(var1 + var2)
+print(var1 - var2)
+print(var1 * var2)
+print(var1 / var2)
---
+++ SYS_5300_C_0913.py
@@ -0,0 +1,30 @@
+print('Hello World')
+
+a=2
+b=3
+print(a+b)
+
+#how to initialize an empty variable
+my_test_variable = None
+print('test')
+
+#what is a constant variable in python
+PI = 3.14
+print(PI)
+PI = 3.15
+print(PI)
+
+my_integer_variable = 1000
+my_float_variable = 3.5
+my_complex_variable = 1 + 2j
+
+print(type(my_integer_variable))
+print(type(my_float_variable))
+print(type(my_complex_variable))
+
+print(2**2)
+#calculates a reminder of a division operation
+print(5%2)
+
+#how to concatenate variables in python
+# +, ''.jion(), format()
